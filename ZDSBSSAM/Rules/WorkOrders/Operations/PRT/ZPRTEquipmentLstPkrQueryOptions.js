e36db0f734ab7ed74d960dc85303b52d350cd05f
kotwaldeepti <deepti.kotwal@cintas.com>
2025-05-26 21:14:26 +0530

    Changes for PRT Equipment Search

--- ZDSBSSAM/Rules/WorkOrders/Operations/PRT/ZPRTEquipmentLstPkrQueryOptions.js
+++ ZDSBSSAM/Rules/WorkOrders/Operations/PRT/ZPRTEquipmentLstPkrQueryOptions.js
@@ -4,36 +4,28 @@ import EnableMultipleTechnician from '../../../../../SAPAssetManager/Rules/SideD
 
 export default function ZPRTEquipmentLstPkrQueryOptions(context) {
     try {
-        // let pageName = libCom.getPageName(context);
-        //     if (searchString) {
-        //         filters.push(`substringof('${searchString.toLowerCase()}', tolower(EquipId))`);
-        //         filters.push(`substringof('${searchString.toLowerCase()}', tolower(Equipment/EquipDesc))`);
-
-        //         filter = `'(' + filters.join(' or ') + ') and PRTFlag eq 'X'`;
-        //     }
-        //     let plantValue = libCom.getUserDefaultPlant();
-        //     let storageLoc = libCom.getUserDefaultStorageLocation();
-        //     return `$orderby=MaterialNum&$expand=Material,MaterialPlant&$filter=${filter}StorageLocation eq '${storageLoc}' and Plant eq '${plantValue}'`;
-        // }
-
         let equipmentLstPkrQueryOptions = context.dataQueryBuilder();
         equipmentLstPkrQueryOptions.expand('SerialNumber');
         equipmentLstPkrQueryOptions.orderBy('EquipId');
-        let newFilterOpts = [`PRTFlag eq 'X'`];
-        equipmentLstPkrQueryOptions.filter(newFilterOpts);
-
-        if (context.searchString) {
-            let searchString = context.searchString;
-            let filters = [
-                `substringof('${searchString.toLowerCase()}', tolower(EquipDesc))`,
-                `substringof('${searchString.toLowerCase()}', tolower(EquipId))`
-            ];
-            //filters = `'(' + filters.join(' or ') + ') and PRTFlag eq 'X'`;
-            equipmentLstPkrQueryOptions.filter(`(${filters.join(' or ') })`);
-            //`(${filters.join(' or ') }) and PRTFlag eq 'X'`)
-
+        if (!context.searchString) {
+            equipmentLstPkrQueryOptions.filter("(PRTFlag eq 'X')");
+            return equipmentLstPkrQueryOptions
         }
-        return equipmentLstPkrQueryOptions;
+        else {
+            let baseQuery = "(PRTFlag eq 'X')";
+            if (context.searchString) {
+                let searchString = context.searchString.toLowerCase();
+                let filters = [
+                    `substringof('${searchString}', EquipId)`,
+                    //`substringof('${searchString}', tolower(EquipDesc))`,
+                ];
+                filters = baseQuery + ' and (' + filters.join(' or ') + ')';
+                equipmentLstPkrQueryOptions.filter(filters);
+                //`(${filters.join(' or ') }) and PRTFlag eq 'X'`)
+
+            }
+            return equipmentLstPkrQueryOptions;
+        }
 
     } catch (exc) {
         // If page is first loaded, attempts to get controls will fail
